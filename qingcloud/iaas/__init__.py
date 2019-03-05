12c15c8f87cfe03a37cdb287932e3329e51ce232
caolinfeng1986@126.com <caolinfeng1986@126.com>
2019-03-05 15:53:40 +0800

    add host port protocol parameters in the function connect_to_zone

--- qingcloud/iaas/__init__.py
+++ qingcloud/iaas/__init__.py
@@ -5,9 +5,9 @@ interface to the IaaS service from QingCloud.
 from qingcloud.iaas.connection import APIConnection
 
 
-def connect_to_zone(zone, access_key_id, secret_access_key, lowercase=True):
+def connect_to_zone(zone, access_key_id, secret_access_key, host,port,protocol,lowercase=True):
     """ Connect to one of zones in qingcloud by access key.
     """
     if lowercase:
         zone = zone.strip().lower()
-    return APIConnection(access_key_id, secret_access_key, zone)
+    return APIConnection(access_key_id, secret_access_key, zone,host,port,protocol)
